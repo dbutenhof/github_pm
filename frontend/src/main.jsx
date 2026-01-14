3cf752698f714ac6924db3ad5d3af69563fa0413
David Butenhof <dbutenho@redhat.com>
2026-01-14 11:56:45 -0500

    Add an app icon (#4)
    Used Gemini to generate an application icon, and created a favicon from that.

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,6 +3,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import '@patternfly/react-core/dist/styles/base.css';
 import App from './App';
+import faviconUrl from './assets/favicon.ico';
+
+// Set favicon dynamically
+const link =
+  document.querySelector("link[rel~='icon']") || document.createElement('link');
+link.rel = 'icon';
+link.type = 'image/png';
+link.href = faviconUrl;
+document.getElementsByTagName('head')[0].appendChild(link);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
